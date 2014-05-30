fbcbe10ee4595281e50a04bf9de6fb3c259cb3ef
Patrick Snape <patricksnape@gmail.com>
2014-05-30 15:49:42 +0100

    Remove unused tests

--- tests/tests.js
+++ tests/tests.js
@@ -2,10 +2,9 @@ define([
   "../lib/latex-log-parser",
   "text!logs/errors.log",
   "text!logs/warnings.log",
-  "text!logs/bad-boxes.log",
-  "text!logs/bad-documentclass.log"
+  "text!logs/bad-boxes.log"
 ],
-function(LatexParser, errorLog, warningLog, badBoxesLog, badDocumentClassLog) {
+function(LatexParser, errorLog, warningLog, badBoxesLog) {
 
 function prettyFileList(files, depth) {
     depth = depth || "  ";
@@ -88,25 +87,6 @@ test("Warning parsing", function() {
   expect(expectedErrors.length);
   for (var i = 0; i < errors.length; i++) {
     if (expectedErrors.indexOf([errors[i].line, errors[i].message, errors[i].file] + "") > -1) {
-      ok(true, "Found error: " + errors[i].message);
-    }
-  }
-});
-
-module("Missing Document Class");
-
-test("Parse Errors", function() {
-  var errors = LatexParser.parse(badDocumentClassLog).errors;
-
-  expectedErrors = [
-    [2, "LaTeX Error: File `aricle.cls' not found."] + "",
-    [null, " ==> Fatal error occurred, no output PDF file produced!"] + ""
-  ];
-
-  expect(expectedErrors.length);
-  for (var i = 0; i < errors.length; i++) {
-    console.log([errors[i].line, errors[i].message]);
-    if (expectedErrors.indexOf([errors[i].line, errors[i].message] + "") > -1) {
       ok(true, "Found error: " + errors[i].message);
     }
   }
